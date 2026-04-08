9820b90047b6c8daf0c0c565f20bd7481cb6bfd7
yellow-lasso <10663657@uvu.edu>
2026-04-07 23:04:09 -0600

    models.py update

--- models.py
+++ models.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    trades = db.relationship("Trade", backref="owner", lazy=True)
+
+
 class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     representative = db.Column(db.String(100), nullable=False)
@@ -14,3 +22,5 @@ class Trade(db.Model):
     amount_min = db.Column(db.String(20), nullable=False)
     amount_max = db.Column(db.String(20), nullable=False)
     source = db.Column(db.String(200), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
